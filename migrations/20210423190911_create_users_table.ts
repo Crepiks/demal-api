654258011b1d35d1ac580e315b52f5e45f5f62ca
Crepiks <sayazhan.onlassyn@mail.ru>
2021-04-23 19:19:43 +0600

    feat: implement create_users_table migration

--- migrations/20210423190911_create_users_table.ts
+++ migrations/20210423190911_create_users_table.ts
@@ -1,10 +1,17 @@
-import * as Knex from "knex";
-
+import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
+  return knex.schema.createTable('users', (table) => {
+    table.increments('id');
+    table.string('firstName', 255).notNullable();
+    table.string('lastName', 255).notNullable();
+    table.string('email', 255).notNullable();
+    table.timestamp('emailConfirmedAt').nullable().defaultTo(null);
+    table.string('password', 255).notNullable();
+    table.timestamp('createdAt').defaultTo(knex.fn.now());
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  return knex.schema.dropTable('users');
 }
-
