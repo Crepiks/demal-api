bfeac23b8be1a9de5188504a4569f7cec25b1693
Crepiks <sayazhan.onlassyn@mail.ru>
2021-04-24 14:05:14 +0600

    feat: implement create_event_images_table migration

--- migrations/20210424134138_create_event_images_table.ts
+++ migrations/20210424134138_create_event_images_table.ts
@@ -1,17 +1,17 @@
 import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('participants', (table) => {
+  return knex.schema.createTable('event_images', (table) => {
     table.increments('id');
-    table.integer('userId').unsigned().notNullable();
-    table.foreign('userId').references('users.id').onDelete('CASCADE');
     table.integer('eventId').unsigned().notNullable();
     table.foreign('eventId').references('events.id').onDelete('CASCADE');
-    table.unique(['userId', 'eventId']);
+    table.integer('imageId').unsigned().notNullable();
+    table.foreign('imageId').references('images.id').onDelete('CASCADE');
+    table.unique(['eventId', 'imageId']);
     table.timestamp('createdAt').defaultTo(knex.fn.now());
   });
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable('participants');
+  return knex.schema.dropTable('event_images');
 }
